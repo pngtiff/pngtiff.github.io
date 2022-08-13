bc0982d8095cf3346703b1452fbaaf31900bc22f
Tiffany P <tiffanypangtc@gmail.com>
2022-08-13 17:02:14 +0900

    PLP project redone in react js

---
+++ PLP/src/App.js
@@ -0,0 +1,22 @@
+import './style.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Signup from './components/Signup'
+import Intro from './components/Intro'
+import Youtube from './components/Youtube'
+import Pricing from './components/Pricing';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Signup />
+      <Intro />
+      <Youtube />
+      <Pricing />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
